f08570766da4d6d876517f7dda7aa0d82706c6e1
Brandon Scott Delap <31800605+baronjr2@users.noreply.github.com>
2019-02-19 18:39:17 -0500

    Add files via upload
    Updated version. Month info is generated on form load.

--- Calendar/DateInfo.cs
+++ Calendar/DateInfo.cs
@@ -3,22 +3,31 @@ public static class Globals
     {
             public static int currentMonth = 0;
             public static string first = "";
+            public static int currMonLen = 0;
+            public static 
             int[] numDays = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
             String[] monthsArr = { "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" };
     }
-public void DateInfo.cs
+public static class DateInfo.cs
 {
 //ini of code for date
 DateTime now = DateTime.Now;
 string month = DateTime.Now.ToString("MM");
 var FirstDay = new DateTime(now.Year, now.Month, 1);
+
+//Gets how long a month is
 var LastDay = FirstDay.AddMonths(1).AddDays(-1);
+
 string firstDate = FirstDay.ToString("dddd");
-   
-//On Load sets the first date of the current month         
+int lastDateNum = Int32.Parse(LastDay.ToString("dd"));
+//On Load sets the first date of the current month
+
+Globals.currentMonth = Int32.Parse(month);
+Globals.currentMonth--;
 Globals.currentMonth = Int32.Parse(month);
 Globals.currentMonth--;
 Globals.first = firstDate;
+Globals.currMonLen = LastDay;
 //Changes label to be on month, can change or delete later            
 MonthLabel.Text = monthsArr[Globals.currentMonth];
 }
