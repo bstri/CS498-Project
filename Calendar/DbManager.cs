2d7f42ecd302706776842a8ccaea1e83792fb8dc
Benjamin Stringer <benjamin.stringer@uky.edu>
2019-02-27 23:42:31 -0500

    Move DbManager and its test back inside the Calendar folder

--- Calendar/DbManager.cs
+++ Calendar/DbManager.cs
@@ -4,6 +4,24 @@ using System.Linq;
 using System.Text;
 using System.Data.SQLite;
 
+    public class sql_row
+    {
+        public sql_row() { }
+
+        public sql_row(string apt_dt, string descr)
+        {
+            apt_date = apt_dt;
+            desc = descr;
+        }
+
+        // Properties.
+        public string apt_date { get; set; }
+        public string desc { get; set; }
+
+        //public override string ToString() => FirstName + "  " + ID;
+    }
+
+
 public class sql_class
 {
     public static void initialize_db()
@@ -23,12 +41,40 @@ public class sql_class
         //Open database
         cal_dbconnection.Open();
         //Create appointments table
-        sql_str = "CREATE TABLE appointments (apdt VARCHAR(10), seqno AUTONUMBER, desc VARCHAR(250), PRIMARY KEY (apdt, seqno));";
+        sql_str = "CREATE TABLE appointments (apdt VARCHAR(10), desc VARCHAR(250), primary key (apdt, desc));";
         sql_cmd = new SQLiteCommand(sql_str, cal_dbconnection);
         //This is the execute that returns an integer representing rows affected
         sql_cmd.ExecuteNonQuery();
         //Close database/connection
         cal_dbconnection.Close();
+
+    }
+
+    public static List<sql_row> get_appointments(string apt_date)
+    {
+
+        SQLiteConnection cal_dbconnection; //connection object
+        string sql_str = "SELECT apdt, desc FROM appointments WHERE apdt = '" + apt_date + "';";
+        SQLiteCommand sql_cmd;
+
+
+        cal_dbconnection = new SQLiteConnection("Data Source=Calendar_db.sqlite;Version=3;");
+        //Open database
+        cal_dbconnection.Open();
+        //insert new appointment
+        sql_cmd = new SQLiteCommand(sql_str, cal_dbconnection);
+        //This is the execute that returns an integer representing rows affected
+        SQLiteDataReader sql_rdr = sql_cmd.ExecuteReader();
+        //Close database/connection
+        var ret_list = new List<sql_row>();
+        while (sql_rdr.Read())
+        {
+            //Console.WriteLine(Convert.ToInt64(sql_rdr["seqno"]));
+            sql_row tmp_row = new sql_row(sql_rdr["apdt"].ToString(), (string)sql_rdr["desc"]);
+            ret_list.Add(tmp_row);
+        }
+        cal_dbconnection.Close();
+        return ret_list;
 
     }
 
@@ -54,30 +100,6 @@ public class sql_class
         cal_dbconnection.Close();
         return row_num;
 
-    }
-
-    public static string select_appointment(string apt_date, int apt_seqno)
-    {
-
-        SQLiteConnection cal_dbconnection; //connection object
-        string sql_str = "SELECT desc FROM appointments WHERE apdt = '" + apt_date + "';";
-        SQLiteCommand sql_cmd;
-
-
-        cal_dbconnection = new SQLiteConnection("Data Source=Calendar_db.sqlite;Version=3;");
-        //Open database
-        cal_dbconnection.Open();
-        //insert new appointment
-        sql_cmd = new SQLiteCommand(sql_str, cal_dbconnection);
-        //This is the execute that returns an integer representing rows affected
-        SQLiteDataReader sql_rdr = sql_cmd.ExecuteReader();
-        //Close database/connection
-
-        sql_rdr.Read();
-
-        string ret_str = (string)sql_rdr["desc"];
-        cal_dbconnection.Close();
-        return ret_str;
     }
 }
 
---
+++ Calendar/test_DbManager.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SQLite;
+
+namespace sqlite_proj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            string sql_str = "";
+            int row_int = 0;
+            //Test initializing db fct
+            sql_class.initialize_db();
+            //Test inserting appointment fct
+            row_int = sql_class.insert_appointment("1993-12-25", "Jacks birth");
+            if (row_int == 1)
+            {
+                Console.WriteLine("row_int was 1");
+            }
+            else
+            {
+                Console.WriteLine("row_int was not 1. It was: " + row_int.ToString());
+            }
+            //Testing selecting appointment fct
+            //sql_str = sql_class.select_appointment("1993-12-25");
+            Console.WriteLine(sql_str);
+            sql_class.insert_appointment("1993-12-25", "Cool dude");
+            sql_class.insert_appointment("1993-12-25", "test");
+            sql_class.insert_appointment("1993-12-25", "test2");
+            List<sql_row> ret_list = sql_class.get_appointments("1993-12-25");
+            foreach(sql_row row in ret_list)
+            {
+                Console.WriteLine(row.apt_date + ' ' + row.desc);
+            }
+            Console.ReadKey();
+        }
+    }
+}
