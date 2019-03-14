94f90959eda1636f0dfaa720673b60265a2f3643
Jack Jaquish <jtja234@uky.edu>
2019-03-14 17:09:59 -0400

    Updated DbManager.cs and test_DbManager.cs. Added fields to appointment table, modified test functions to work with changes.

--- test_DbManager.cs
+++ test_DbManager.cs
@@ -9,13 +9,11 @@ namespace sqlite_proj
     class Program
     {
         static void Main(string[] args)
-        { 
+        {
             string sql_str = "";
             int row_int = 0;
             //Test initializing db fct
             sql_class.initialize_db();
-            //Test inserting appointment fct
-            row_int = sql_class.insert_appointment("1993-12-25", "Jacks birth");
             if (row_int == 1)
             {
                 Console.WriteLine("row_int was 1");
@@ -27,13 +25,13 @@ namespace sqlite_proj
             //Testing selecting appointment fct
             //sql_str = sql_class.select_appointment("1993-12-25");
             Console.WriteLine(sql_str);
-            sql_class.insert_appointment("1993-12-25", "Cool dude");
-            sql_class.insert_appointment("1993-12-25", "test");
-            sql_class.insert_appointment("1993-12-25", "test2");
-            List<sql_row> ret_list = sql_class.get_appointments("1993-12-25");
-            foreach(sql_row row in ret_list)
+            //Specify year, month, day, time, descr to insert row
+            sql_class.insert_appointment(1993, 14, 25, "12:30", "Cool dude");
+            //Specify year, month, date to get appointments
+            List<sql_row> ret_list = sql_class.get_appointments(1993, 13, 25);
+            foreach (sql_row row in ret_list)
             {
-                Console.WriteLine(row.apt_date + ' ' + row.desc);
+                Console.WriteLine(row.apt_year + ' ' + row.apt_month + ' ' + row.apt_day + ' ' + row.apt_time + ' ' + row.desc);
             }
             Console.ReadKey();
         }
