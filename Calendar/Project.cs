993c762b08525f80daac37e1c4eb2b579411b0a3
bstri <benjamin.stringer@uky.edu>
2019-04-05 10:34:31 -0400

    Adds Project.cs class

---
+++ Calendar/Project.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calendar
+{
+    public class Project
+    {
+        public readonly string Name;
+        public readonly string Description;
+        public readonly DateTime Start;
+        public readonly DateTime End;
+        public readonly ConsoleColor Color;
+
+        public Project(string name, DateTime start, DateTime end, ConsoleColor color, string description = "")
+        {
+            Name = name;
+            Start = start;
+            End = end;
+            Color = color;
+            Description = description;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || GetType() != obj.GetType())
+                return false;
+
+            Project p2 = (Project)obj;
+            return (Name == p2.Name && Start == p2.Start && End == p2.End && Color == p2.Color && Description == p2.Description);
+        }
+
+        public override int GetHashCode()
+        {
+            return Start.GetHashCode();
+        }
+    }
+}
