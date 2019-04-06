cca4db69692a023230291e81cd921de39728cfe5
bstri <benjamin.stringer@uky.edu>
2019-04-06 13:19:40 -0400

    small fixes to AddProjectForm

--- Calendar/addProjectForm.cs
+++ Calendar/addProjectForm.cs
@@ -33,24 +33,18 @@ namespace Calendar
             else
             {
                 EmptyProjectNameWarningLabel.Visible = false;
-                //todo
-                //Add SQL database project creation
 
                 DateTime start = ProjDateStart.Value;
                 DateTime end = ProjectDateEnd.Value;
-                if((Int32.Parse(start.ToString("dd")) > Int32.Parse(end.ToString("dd")))|| (Int32.Parse(start.ToString("yyyy")) > Int32.Parse(end.ToString("yyyy")))|| Int32.Parse(start.ToString("MM")) > Int32.Parse(end.ToString("MM")))
+                if(start > end)
                 {
                     MessageBox.Show("Please enter valid dates!");
                     return;
                 }
+                //todo add the project to the database
                 //Event newEvent = new Event(EventNameTextBox.Text, combined, EventDescriptionTextBox.Text);
                 //sql_class.AddEvent(newEvent);
-                MonthlyView mv = new MonthlyView(true);
-                //DateTime refreshDate = new DateTime(date.Year, date.Month, date.Day);
-                //mv.RefreshDayFrame(refreshDate, newEvent);
-                //MonthlyView.RefreshDayFrame(refreshDate, true, newEvent);
-                //mv.RefreshDayFrame(refreshDate, true, newEvent);
-                //DayFrame d = MonthlyView.dayFrames[0];
+                
                 this.Close();
             }
         }
